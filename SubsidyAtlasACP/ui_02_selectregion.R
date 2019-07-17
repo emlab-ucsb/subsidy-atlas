8d2d88a3f9e4c89370903b4502ce2d71e686fbb1
kmillage <millagek@gmail.com>
2019-07-17 15:41:35 -0700

    regional map with clicking

---
+++ SubsidyAtlasACP/ui_02_selectregion.R
@@ -0,0 +1,22 @@
+### -----------------------------------
+#
+# UI - Select a region
+# 
+### ------------------------------------
+
+### Function
+selectregion = function()
+  fluidPage(
+    
+    # Header and introductory text
+    column(12, style = "padding: 15px 25px; border-top: 4px solid #3c8dbc;",
+           tags$h3(style = "color: #ffffff; padding: 0; margin: 0;", "ACP Regions")
+    ), 
+    
+    # Leaflet overview map
+    column(12, stlye = "padding: 0px;",
+           leafletOutput("regional_map", width = "auto", height = "80vh")
+    )
+    
+    
+  ) # close fluidPage
