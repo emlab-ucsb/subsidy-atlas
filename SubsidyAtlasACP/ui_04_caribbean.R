b0cdd853b786ccbef6bb202a272a9501f31012e2
kmillage <millagek@gmail.com>
2019-08-01 17:04:33 -0700

    tab aesthetics

--- SubsidyAtlasACP/ui_04_caribbean.R
+++ SubsidyAtlasACP/ui_04_caribbean.R
@@ -9,16 +9,32 @@
 caribbean = function(caribbean_eez_choices)
   fluidPage(style = "color: #ffffff; padding-bottom: 40px;",
             
-            tags$style("
-                                
-                                .nav-tabs-custom .nav-tabs li.active:hover a, .nav-tabs-custom .nav-tabs li.active a {
-                                background-color: transparent;
-                                border-color: transparent;
-                                }
-                                
-                                .nav-tabs-custom .nav-tabs li.active {
-                                border-top-color: #FFF;
-                                }"),
+            # Tabs custom styling - for some reason this has to be here
+            tags$style(
+".nav-tabs {
+background: black;
+}
+
+.nav-tabs-custom .nav-tabs {
+border-bottom-color: transparent;
+}
+
+.nav-tabs-custom .nav-tabs li.active:hover a, .nav-tabs-custom .nav-tabs li.active a {
+color: #ffffff;
+background: #1A1B1D;
+}
+
+.nav-tabs-custom .nav-tabs li.active {
+border-top-color: #3c8dbc;
+}
+
+.nav-tabs-custom .nav-tabs li.active a {
+background: #1A1B1D;
+color: #ffffff;
+border-left-color: transparent;
+border-right-color: transparent;}"
+
+            ),
     
     # Top header
     column(12, style = "padding: 15px 25px; border-top: 4px solid #3c8dbc;",
@@ -61,39 +77,51 @@ caribbean = function(caribbean_eez_choices)
     
     ### Tab box with results
     
+    column(12, style = "width: 95%; margin: 0 2.5% 0 2.5%;",
+           
     tabBox(width = 12, id = "caribbean_tabs", 
            
            # First tab - connectivity map
            tabPanel("Origins of distant water fishing vessels", 
                          
-                    column(12, style = "padding: 15px 0;",
+                    column(12, style = "padding: 15px 0; 
+                           border-style: solid;
+                           border-width: 2px 1px 1px 1px;
+                           border-color: #1A1B1D;",
                            
                            leafletOutput("caribbean_connection_map",
-                                         width = "95%",
+                                         width = "100%",
                                          height = "50vh")
                     )
            ),
              
            # Second tab - heat maps   
            tabPanel("Fishing effort and subsidy intensity of distant water vessels",
+                    column(12, style = "padding: 15px 25px; 
+                           border-style: solid;
+                           border-width: 2px 1px 1px 1px;
+                           border-color: #1A1B1D;",
+                           
                     fluidRow(
                            
-                      column(6, style = "padding: 15px 25px; background-color: #262626;",
+                      column(6,
                                 
                              plotOutput("caribbean_subsidy_map", 
                                         width = "auto")
                                 
                       ),
                          
-                      column(6, style = "padding: 15px 25px; background-color: #262626;",
+                      column(6,
                                 
                              plotOutput("caribbean_effort_map",
                                         width = "auto")
                                 
                       )   
                     ) #/fluidRow
+                    )
            ) #/tabPanel #2
                 
     ) # /tabBox
+    )
     
   ) #close fluid page
