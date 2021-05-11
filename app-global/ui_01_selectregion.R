e2fed16ac8171b3b217453dfc735e50781f6b868
kmillage <millagek@gmail.com>
2021-05-11 15:42:57 -0700

    front page overview up and FAQs page seems to be working

--- app-global/ui_01_selectregion.R
+++ app-global/ui_01_selectregion.R
@@ -17,27 +17,29 @@ selectregion = function(){
            leafletOutput("regional_map", width = "auto", height = "90vh"),
            
            # Text panel
-           # Button to hide disclaimer 
+           # Button to hide instructions 
            absolutePanel(id = "regional_map_hide_text_arrow",
                                          
                                          tags$button(id = "ab_regional_map_hide_text",
                                                      class = "btn action-button",
-                                                     icon("caret-down"))
+                                                     "Instructions", icon("caret-down"))
            ),
            
-           # Button to open left panel
+           # Button to show instructions
            shinyjs::hidden(absolutePanel(id = "regional_map_expand_text_arrow",
                          
                          tags$button(id = "ab_regional_map_expand_text",
                                      class = "btn action-button",
-                                     icon("caret-up"))
+                                     "Instructions", icon("caret-up"))
            )),
            
+           # Instructions
            absolutePanel(id = "regional_map_text_panel",
                                          
                          column(12, id = "tb-spaced-div", align = "center",
                                 
-                                includeHTML("./text/intro_overlay.html")
+                                tags$p("Select a region by clicking on the map or using the links in the left panel to view more information about distant water fishing activity in that area. Please be patient if nothing is visible – the map may take a couple of seconds to load.")
+                                #includeHTML("./text/intro_overlay.html")
                                                 
                          )
                                          
